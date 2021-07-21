2cc4582e21db6537790ab223a2633d06b3e80896
b2pacific <b2pacific@gmail.com>
2021-07-21 23:28:28 +0530

    Removed logs

--- index.js
+++ index.js
@@ -17,53 +17,52 @@ app.use(cors());
 io.on("connection", (socket) => {
   console.log("user connected");
   socket.on("initialize", function (data) {
-    console.log("initialize", data);
+    // console.log("initialize", data);
 
     socket.join(data);
 
+    socket.roomCount = io.sockets.adapter.rooms.get(data).size;
 
-      socket.roomCount = io.sockets.adapter.rooms.get(data).size;
-
-    
     io.to(data).emit("update-members", io.sockets.adapter.rooms.get(data).size);
     socket.room = data;
-    
+
     io.to(data).emit("get-data", socket.id);
   });
   socket.on("send-data", ({ value, id }) => {
-    console.log(value[0].children[0].text);
+    // console.log(value[0].children[0].text);
     io.to(id).emit("send-data", value);
   });
   socket.on("new-operations", function (data) {
-    console.log(data);
+    // console.log(data);
     io.to(data.id).emit("new-remote-operations", data);
   });
 
   socket.on("disconnecting", function () {
     //if (io.sockets.adapter.rooms.get(socket.room)!=undefined && socket.room!=undefined) {
     //if(io.nsps!=undefined) {
-      console.log(socket.room);
-      console.log(socket.roomCount);
-      socket.roomCount = socket.roomCount - 1;
-      io.to(socket.room).emit(
-        "update-members",
-        socket.roomCount
-      );
-      socket.leave(socket.room);
+    // console.log(socket.room);
+    // console.log(socket.roomCount);
+    socket.roomCount = socket.roomCount - 1;
+    io.to(socket.room).emit("update-members", socket.roomCount);
+    socket.leave(socket.room);
     //}
-    console.log("disconnecting");
+    // console.log("disconnecting");
   });
+});
+
+app.get("/", (req, res) => {
+  return res.send("Hello from Prashant");
 });
 
 app.get("/create", (req, res) => {
   crypto.randomBytes(6, (err, buf) => {
     if (err) console.log("Error", err);
     buf = buf.toString("hex");
-    console.log(buf);
+    // console.log(buf);
     return res.json({ groupId: buf });
   });
 });
 
-server.listen(4000, function () {
-  console.log("Connected on 4000");
+server.listen(process.env.PORT, function () {
+  console.log("Connected on ", process.env.PORT);
 });
